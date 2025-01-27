4bfeeba4c8cefac1323cc8d26e37582b643f0243
MohdShayan <sehbaqureshi@gmail.com>
2025-01-27 10:02:01 +0530

    ini commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+
+    <footer>
+      <div>
+        <p>
+          Made with <span>❤️</span> by TCB
+        </p>
+      </div>
+    </footer>
+    </>
+  )
+}
+
+export default App
